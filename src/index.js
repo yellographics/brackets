b53a405496169335bb6e545448841f265bf2eb01
Anna Nescierowicz <annie.odesk@gmail.com>
2022-02-16 15:14:19 +0300

    feat: implement function. 2 tests fail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  let iterator = 0;
+  let openBrackets = [];
+  let closeBrackets = [];
+  function getElements(array, which, newArray) {
+    array.forEach((elem) => {
+      elem.forEach((symbol, index) => {
+        if (index === which) {
+          newArray.push(symbol);
+        }
+      });
+    });
+  }
+  getElements(bracketsConfig, 0, openBrackets);
+  getElements(bracketsConfig, 1, closeBrackets);
+
+  for (const symbol of str) {
+    if (
+      openBrackets.find((elem) => elem === symbol) &&
+      closeBrackets.find((elem) => elem === symbol)
+    ) {
+      if (!iterator % 2) {
+        stack.push(symbol);
+        iterator++;
+      } else {
+        const indexCloseBracket = closeBrackets.findIndex(
+          (elem) => elem === symbol
+        );
+          const poped = stack.pop();
+          iterator = 0;
+        if (poped !== openBrackets[indexCloseBracket]) {
+          console.log(false);
+          return false;
+        }
+      }
+    } else if (
+      openBrackets.find((elem) => elem === symbol) &&
+      !closeBrackets.find((elem) => elem === symbol)
+    ) {
+      stack.push(symbol);
+    } else if (
+      !openBrackets.find((elem) => elem === symbol) &&
+      closeBrackets.find((elem) => elem === symbol)
+    ) {
+      const indexCloseBracket = closeBrackets.findIndex(
+        (elem) => elem === symbol
+      );
+      const poped = stack.pop();
+      if (poped !== openBrackets[indexCloseBracket]) {
+        console.log(false);
+        return false;
+      }
+    }
+  }
+
+  if (stack.length > 0) {
+    console.log(false);
+    return false;
+  } else {
+    console.log(true);
+    return true;
+  }
+};;
+
+
